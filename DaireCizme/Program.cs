de1e1877b54a4b1a845cde459d4b8054396ed595
buse özdemir <ozdmrbuse1@gmail.com>
2023-02-25 21:45:06 +0300

    Daire Çizme 25.02.2023

---
+++ DaireCizme/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Yarıçap uzunluğu giriniz : ");
+double radius = int.Parse(Console.ReadLine());
+double thickness = 0.8;
+double rin = radius - thickness, rOut = radius + thickness;
+
+   
+
+    for (double y = radius; y >= -radius;y--)
+    {
+        for (double x = -radius; x <= rOut; x += 0.6)
+        {
+            double value = x * x + y * y;
+            if (value >= rin * rin && value <= rOut * rOut)
+            {
+                Console.Write("*");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+Console.Read();
