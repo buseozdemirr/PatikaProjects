7d48885133e0802922db351ac27b0d079d8aeffc
buse özdemir <ozdmrbuse1@gmail.com>
2023-02-25 18:04:21 +0300

    25.02.2023

---
+++ UcgenCizme/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Üçgenin kenar uzunluğu : ");
+int kenarUzunluk=int.Parse(Console.ReadLine());
+for(int i=0;i<=kenarUzunluk;i++)
+{
+	for (int k = 0; k <=kenarUzunluk-i; k++)
+	{
+		Console.Write(" ");
+	}
+	for (int j = 0; j<=i*2 ; j++)
+	{
+		Console.Write("*");
+	}
+	Console.Write("\n");
+}
+Console.Read();
